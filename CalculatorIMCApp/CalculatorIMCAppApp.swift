9e1d57be31def0fed02893a6ddc66b01ce9e9355
brayyanchristophertrujillovalle <brayyantrujil@gmail.com>
2024-06-26 20:25:00 -0500

    Initial Commit

---
+++ CalculatorIMCApp/CalculatorIMCAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  CalculatorIMCAppApp.swift
+//  CalculatorIMCApp
+//
+//  Created by Brayyan Christopher Trujillo Valle on 26/06/24.
+//
+
+import SwiftUI
+
+@main
+struct CalculatorIMCAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
